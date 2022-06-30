bb12a74e44c7a873450dfe289f2ed760a2974aa4
anh.laiviet <anh.laiviet@powergatesoftware.com>
2022-06-30 18:03:21 +0700

    setup redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import { EmptyLayout } from '@/components/layout';
-import { AppPropsWithLayout } from '@/models';
+import { AppPropsWithLayout } from '@/models/index';
 import createEmotionCache from '@/utils/create-emotion-cache';
 import theme from '@/utils/theme';
 import { CacheProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
 import { SWRConfig } from 'swr';
-import axiosClient from '@/axios/axios-client';
+import axiosClient from '../src/api-configure/axios-client';
+import store from '../src/redux/store';
 import '../styles/globals.css';
 
 // Client-side cache, shared for the whole session of the user in the browser.
@@ -19,21 +21,23 @@ function MyApp({
 }: AppPropsWithLayout) {
 	const Layout = Component.Layout ?? EmptyLayout;
 	return (
-		<CacheProvider value={emotionCache}>
-			<ThemeProvider theme={theme}>
-				<CssBaseline />
-				<SWRConfig
-					value={{
-						fetcher: (url) => axiosClient.get(url),
-						shouldRetryOnError: false,
-					}}
-				>
-					<Layout>
-						<Component {...pageProps} />
-					</Layout>
-				</SWRConfig>
-			</ThemeProvider>
-		</CacheProvider>
+		<Provider store={store}>
+			<CacheProvider value={emotionCache}>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<SWRConfig
+						value={{
+							fetcher: (url) => axiosClient.get(url),
+							shouldRetryOnError: false,
+						}}
+					>
+						<Layout>
+							<Component {...pageProps} />
+						</Layout>
+					</SWRConfig>
+				</ThemeProvider>
+			</CacheProvider>
+		</Provider>
 	);
 }
 
